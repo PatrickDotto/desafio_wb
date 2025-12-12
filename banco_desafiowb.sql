b09d6ff16fedd2c513cf930d7c4f92814a46ec7a
Patrick Dotto <patrickdotto05@gmail.com>
2025-12-11 22:07:30 -0300

    update

--- banco_desafiowb.sql
+++ banco_desafiowb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/12/2025 às 00:18
+-- Tempo de geração: 12/12/2025 às 02:05
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -37,16 +37,6 @@ CREATE TABLE `tarefas` (
   `data_limite` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `tarefas`
---
-
-INSERT INTO `tarefas` (`id`, `usuario_id`, `titulo`, `descricao`, `status`, `data_criacao`, `data_limite`) VALUES
-(21, 16, 'ASDAS', 'SADASD', 'pendente', '2025-11-30 19:58:10', '2025-10-28'),
-(22, 16, 'Limpar casa', 'Varrer', 'concluida', '2025-11-30 21:27:35', '2025-11-25'),
-(23, 16, 'Mexer excel', 'PPP', 'concluida', '2025-11-30 21:27:47', '2025-10-10'),
-(24, 16, 'Mexer Word', 'Sim', 'concluida', '2025-11-30 21:28:03', '2025-11-22');
-
 -- --------------------------------------------------------
 
 --
@@ -59,13 +49,6 @@ CREATE TABLE `usuarios` (
   `email` varchar(100) NOT NULL,
   `senha` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(16, 'Patrick', 'patrickdotto05@gmail.com', '$2y$10$1PDbB0wgQKyYncxIhoftDuSbTITjG00v0C0aKqqx.nfnx.7ql44xW');
 
 --
 -- Índices para tabelas despejadas
@@ -93,7 +76,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `tarefas`
 --
 ALTER TABLE `tarefas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
